f458e198a979d20e8a35a76cc2da32dd50d8f651
Rachna-2018 <41289843+Rachna-2018@users.noreply.github.com>
2018-08-14 12:00:21 +0530

    Update index.php

--- index.php
+++ index.php
@@ -59,7 +59,8 @@ if($method == 'POST')
     		curl_setopt_array( $ch, $options );
 		$json = curl_exec( $ch );
 		$someobj = json_decode($json,true);
-		$speech = "$ROOMS bedroom houses are available in metro areas \n" ;
+		if($ROOMS == 0) {$room_text = "Houses ";}else {$room_text = "$ROOMS bedroom houses ";}
+		$speech = $room_text."are available in metro areas \n" ;
 		foreach ($someobj["results"] as $value) 
 		{
 			$speech .= $value["LOCATIONS"];
